3f6249eb258858704d6694fdb276b4c1701d4e84
(Student  C00298918) Harry Knight <C00298918@setu.ie>
2024-11-15 12:39:47 +0000

    Refactor 2: Tidied up

--- 16_SFML_Games/minesweeper.cpp
+++ 16_SFML_Games/minesweeper.cpp
@@ -9,65 +9,80 @@ int minesweeper()
 
     RenderWindow app(VideoMode(400, 400), "Minesweeper!");
 
-    int w=32;
+    int w = 32;
     int grid[12][12];
     int sgrid[12][12]; //for showing
 
-    Texture tilesTexture; // Refactor: I changed the varible name from t to tilesTexture to specify what the varible holds
+    Texture tilesTexture; // Refactor 1: I changed the varible name from t to tilesTexture to specificaly say what the varible holds
     tilesTexture.loadFromFile("images/minesweeper/tiles.jpg"); 
     Sprite s(tilesTexture);
 
-    for (int i=1;i<=10;i++)
-     for (int j=1;j<=10;j++)
-      {
-        sgrid[i][j]=10;
-        if (rand()%5==0)  grid[i][j]=9;
-        else grid[i][j]=0;
-      }
+    // Refactor 2: Tidied up
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 10; j++) {
+            sgrid[i][j] = 10; // Set cell as unrevealed
+            grid[i][j] = (rand() % MINE_CHANCE == 0) ? 9 : 0; // Assign mine with a 1 in MINE_CHANCE
+        }
+    }
+    //
 
-    for (int i=1;i<=10;i++)
-     for (int j=1;j<=10;j++)
+    // Refactor 2: Tidied up
+    for (int i = 1; i <= 10; i++)
+     for (int j = 1; j <= 10; j++)
       {
-        int n=0;
-        if (grid[i][j]==9) continue;
-        if (grid[i+1][j]==9) n++;
-        if (grid[i][j+1]==9) n++;
-        if (grid[i-1][j]==9) n++;
-        if (grid[i][j-1]==9) n++;
-        if (grid[i+1][j+1]==9) n++;
-        if (grid[i-1][j-1]==9) n++;
-        if (grid[i-1][j+1]==9) n++;
-        if (grid[i+1][j-1]==9) n++;
-        grid[i][j]=n;
+        int n = 0;
+        if (grid[i][j] == 9) continue;
+        if (grid[i + 1][j] == 9) n++;
+        if (grid[i][j + 1] == 9) n++;
+        if (grid[i - 1][j] == 9) n++;
+        if (grid[i][j - 1] == 9) n++;
+        if (grid[i + 1][j + 1] == 9) n++;
+        if (grid[i - 1][j - 1] == 9) n++;
+        if (grid[i - 1][j + 1] == 9) n++;
+        if (grid[i + 1][j - 1] == 9) n++;
+        grid[i][j] = n;
       }
+    //
 
     while (app.isOpen())
     {
+        // Refactor 2: Tidied up
         Vector2i pos = Mouse::getPosition(app);
-        int x = pos.x/w;
-        int y = pos.y/w;
+        int x = pos.x / w;
+        int y = pos.y / w;
+        //
 
         Event e;
         while (app.pollEvent(e))
         {
-            if (e.type == Event::Closed)
+            // Refactor 2: Tidied up
+            if (e.type == Event::Closed) {
                 app.close();
+            }
 
-            if (e.type == Event::MouseButtonPressed)
-              if (e.key.code == Mouse::Left) sgrid[x][y]=grid[x][y];
-              else if (e.key.code == Mouse::Right) sgrid[x][y]=11;
+            if (e.type == Event::MouseButtonPressed) {
+                if (e.key.code == Mouse::Left) {
+                    sgrid[x][y] = grid[x][y];
+                }
+                else if (e.key.code == Mouse::Right) {
+                    sgrid[x][y] = 11;
+                }
+            //
+            }
         }
 
         app.clear(Color::White);
 
-        for (int i=1;i<=10;i++)
-         for (int j=1;j<=10;j++)
+        // Refactor 2: Tidied up
+        for (int i = 1;i <= 10; i++)
+         for (int j = 1; j <= 10; j++)
           {
-           if (sgrid[x][y]==9) sgrid[i][j]=grid[i][j];
-           s.setTextureRect(IntRect(sgrid[i][j]*w,0,w,w));
-           s.setPosition(i*w, j*w);
+           if (sgrid[x][y] == 9) sgrid[i][j] = grid[i][j];
+           s.setTextureRect(IntRect(sgrid[i][j] * w,0,w,w));
+           s.setPosition(i * w, j * w);
            app.draw(s);
           }
+        //
 
         app.display();
     }
